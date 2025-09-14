6ac6a18182111e3b66f997ece1e528ac898cf722
Zach Weineich <weinreych@gmail.com>
2025-09-14 09:20:03 -0500

    add snake component

--- personal-project/src/app/app-routing.module.ts
+++ personal-project/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 // import components
 import { HomeComponent } from './home/home.component';
 import { WordleComponent } from './wordle/wordle.component';
+import { SnakeComponent } from './snake/snake.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'wordle', component: WordleComponent },
+  { path: 'snake', component: SnakeComponent },
 ];
 
 @NgModule({
---
+++ personal-project/src/app/snake/snake.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snake',
+  imports: [],
+  templateUrl: './snake.component.html',
+  styleUrl: './snake.component.scss'
+})
+export class SnakeComponent {
+
+}
