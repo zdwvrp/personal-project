f6b77b01cc80b4131fa2e1e4689bcf75a4e6d595
Zach Weineich <weinreych@gmail.com>
2026-01-04 13:14:16 -0600

    Track player life totals with logs & show life difference when damaging or healing

--- personal-project/src/app/mtg-life-counter/mtg-life-counter.component.ts
+++ personal-project/src/app/mtg-life-counter/mtg-life-counter.component.ts
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class MtgLifeCounterComponent {
-  players: number = 4;
-  playerArray: Array<Object> = [];
+  game: any = {};
 
   startingLifeTotal: number = 40;
   startingLifeTotalOptions: Array<number> = [20, 40];
@@ -29,25 +28,22 @@ export class MtgLifeCounterComponent {
 
   lifeChangeState = {
     enabled: false,
-    initiator: null
+    initiator: null,
+    editableGameState: null,
   };
   lifeChangeInitiatorMap: {
     0: 'damage',
     1: 'heal'
   };
-  lifeChangeEnabled: boolean = false;
 
   ngOnInit() {
     this.actionButtonState = 'code';
-    const ongoingGame = localStorage.getItem("players");
+    const ongoingGame = localStorage.getItem("game");
     if (ongoingGame) {
       this.loadGameSnapshotFromLocalStorage();
     } else {
       // Instantiate New Game
-      this.playerArray = [];
-      for(var i = 0; i < this.players; i++) {
-        this.addPlayerToPlayerArray();
-      }
+      this.instantiateNewGame();
     }
 
   }
@@ -57,34 +53,81 @@ export class MtgLifeCounterComponent {
   ///////////////////
 
   takeGameSnapshot() {
-    const players = {players: [...this.playerArray]};
-    const playersString = JSON.stringify(players);
-    localStorage.setItem("players", playersString);
+    const gameString = JSON.stringify(this.game);
+    localStorage.setItem("game", gameString);
   }
 
   loadGameSnapshotFromLocalStorage() {
-    const players = localStorage.getItem("players");
-    this.playerArray = JSON.parse(players).players;
+    const ongoingGame = JSON.parse(localStorage.getItem("game"));
+    this.game = { ...ongoingGame };
     console.log("INFO | Session restored!!");
   }
 
+
+  ////////////////////
+  /* Game Functions */
+  ////////////////////
+
+  instantiateNewGame() {
+    console.log("INFO | Instantiating new game...");
+    this.game.numberOfPlayers = 4;
+    this.game.players = [];
+    for(var i = 0; i < this.game.numberOfPlayers; i++) {
+      this.addPlayerToGame();
+    }
+
+    this.game.logs = []; // Stack; Keeps track of damage logs that can be undone if desired.
+  }
+
+  createGameLog(dealingPlayerId: number, playerLifeChanges: Array<any>) {
+    const newGameLog = {
+      dealingPlayerId: dealingPlayerId,
+      playerLifeChanges: playerLifeChanges
+    };
+
+    this.game.logs.push(newGameLog); // Add log to game object.
+    this.takeGameSnapshot(); // Take new snapshot here.
+  }
 
   //////////////////////
   /* Player Functions */
   //////////////////////
 
-  addPlayerToPlayerArray() {
-    this.playerArray.push(this.getNewPlayerObj());
+  addPlayerToGame() {
+    this.game.players.push(this.getNewPlayerObj());
+  }
+  removePlayerFromGame(playerId: number | null = null) {
+    if (playerId !== null) {
+      // remove the specified player //
+    } else {
+      this.game.players.pop();
+    }
   }
 
   getNewPlayerObj() {
     return {
       // player properties here
       life: this.startingLifeTotal,
+      id: this.game.players.length,
     }
   }
 
-
+  damagePlayer(player) {
+    if (this.lifeChangeState.enabled) {
+      this.lifeChangeState.editableGameState.players[player.id].life--;
+    } else {
+      console.log("ERROR | Attempted to damage player while life change is disabled...");
+    }
+    
+  }
+  healPlayer(player) {
+    if (this.lifeChangeState.enabled) {
+      this.lifeChangeState.editableGameState.players[player.id].life++;
+    } else {
+      console.log("ERROR | Attempted to heal player while life change is disabled...");
+    }
+    
+  }
 
 
   //////////////////
@@ -98,9 +141,11 @@ export class MtgLifeCounterComponent {
       // Perform different actions based on newState
       switch(newState) {
         case 'code':
+          // Collapse action menu
           this.actionMenuHidden = true;
           break;
         case 'code_off':
+          // Expand action menu
           this.actionMenuHidden = false;
           break;
         case 'check':
@@ -122,18 +167,14 @@ export class MtgLifeCounterComponent {
         this.changeActionButtonState('code');
         break;
       case 'check':
+        if (this.lifeChangeState.enabled) {
+          this.toggleLifeChange(null);
+        }
         // End current action & trigger things accordingly
         break;
       default:
         console.error('Unknown current action button state "', this.actionButtonState, '"...');
     }
-  }
-
-  damagePlayer(player) {
-    player.life--;
-  }
-  healPlayer(player) {
-    player.life++;
   }
 
   toggleLifeChange(initiator: string | null) {
@@ -146,17 +187,50 @@ export class MtgLifeCounterComponent {
       this.actionButtonCurrentAction = null;
       this.changeActionButtonState('code_off');
 
-      // Take snapshot
-      this.takeGameSnapshot();
+      // Create game log
+      const playerLifeChanges = [];
+      this.game.players.forEach((player, pid) => {
+        if (player.life !== this.lifeChangeState.editableGameState.players[pid].life) {
+          playerLifeChanges.push({
+            playerId: player.id,
+            prevLife: player.life,
+            newLife: this.lifeChangeState.editableGameState.players[pid].life
+          });
+        }
+      });
+
+      if (playerLifeChanges.length > 0) {
+        this.createGameLog(0, playerLifeChanges);
+      }
+
+      // Adjust live life totals accordingly
+      this.game.players.forEach((player, pid) => {
+        player.life = this.lifeChangeState.editableGameState.players[pid].life;
+      });
+
+      // Lastly, reset the editable game state for next use.
+      this.lifeChangeState.editableGameState = null;
     } else {
       // Allow player health to be altered
       this.lifeChangeState.enabled = !this.lifeChangeState.enabled;
       this.lifeChangeState.initiator = initiator;
 
+      this.lifeChangeState.editableGameState = {};
+      this.lifeChangeState.editableGameState.players = JSON.parse(JSON.stringify(this.game.players));
+
       // Set actionButton values
       this.actionButtonCurrentAction = initiator;
       this.changeActionButtonState('check');
     }
+  }
+
+  getLifeChangeDifference(playerId) {
+    if (this.lifeChangeState.enabled) {
+      const lifeChange = this.lifeChangeState.editableGameState.players[playerId].life - this.game.players[playerId].life;
+      console.log("INFO | getLifeChangeDifference(", playerId, ") => ", this.game.players[playerId].life, " - ", this.lifeChangeState.editableGameState.players[playerId].life, " = ", lifeChange);
+      return this.lifeChangeState.editableGameState.players[playerId].life - this.game.players[playerId].life;
+    }
+    return null;
   }
 }
 
